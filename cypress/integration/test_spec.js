8785acddef671a1ce8ec2160dcb7e45a02a61fc3
Tim Arney <timarney@users.noreply.github.com>
2017-01-10 14:56:22 -0500

    Update test_spec.js

--- cypress/integration/test_spec.js
+++ cypress/integration/test_spec.js
@@ -1,6 +1,6 @@
 const page = 'http://localhost:3000'
 
-describe('MobX Render Test', function(){
+describe('Preact Rewire Render Test', function(){
   it('Assert page rendered', function(){
     cy.visit(page)
     cy.get('.App-header').find('h2').should("have.text", "Welcome to Preact")
